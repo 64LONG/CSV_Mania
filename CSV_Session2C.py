7a8f862e091d916fa64e993e20c2cd54f52fa30c
64LONG <64298723+64LONG@users.noreply.github.com>
2025-10-31 08:04:28 -0500

    Create CSV_Session2C.py
    CSV_Session2C

---
+++ CSV_Session2C.py
@@ -0,0 +1,25 @@
+import sys
+import csv
+
+def csv_reader(filename):
+  """Read and output the details of CSV file."""
+  try:
+    print("1")
+    with open(filename) as csv_file:
+      print("2")
+      csv_data=csv.reader(csv_file)
+      print("3")
+      csv_data
+      for csv_record in csv_data:
+        print(csv_record)
+      csv_file.close()
+  except:
+    print ("Unable to find file named" , filename)
+
+try:
+  print('Processing CSV file')
+  csv.reader('/<Enter path to the .csv file here>')
+except:
+  print ("Unable to find file")
+
+#input ('Please press ENTER to exit')
