ae7afc2eaa9a43bd8b319872a3348a6fd9bca2d8
64LONG <64298723+64LONG@users.noreply.github.com>
2025-10-31 07:02:44 -0500

    Create CSV_Session0.py
    CSV_Session0

---
+++ CSV_Session0.py
@@ -0,0 +1,9 @@
+import sys
+import csv
+
+#dataFile = '/<Enter path to the .csv file here>'
+#csvReader = csv.reader(dataFile)
+#print(csvReader)
+
+csvReader = csv.reader('/<Enter path to the .csv file here>')
+print(csvReader)
